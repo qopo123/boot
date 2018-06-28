f7aeffa0db56a70329897209b5e99c65b8ea0b70
yangchuanhua <yangchuanhua@yonyou.com>
2018-06-28 22:37:01 +0800

    增加注释

--- src/main/java/com/hua/springboot/boot/proxy/DynamicProxy.java
+++ src/main/java/com/hua/springboot/boot/proxy/DynamicProxy.java
@@ -7,14 +7,13 @@ public class DynamicProxy {
 
 
   public static void main(String[] args) {
+    //被代理类
     ForumService forumService = new ForumServiceImpl();
-
+    //handler处理类
     ProxyHandler proxyHandler = new ProxyHandler(forumService);
-
+    //实现形式
     ForumService  o = (ForumService) Proxy.newProxyInstance(forumService.getClass().getClassLoader(),
         forumService.getClass().getInterfaces(), proxyHandler);
-
-
     o.removeForum(11);
     o.removeTopic(11);
 
